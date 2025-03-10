5d40cf687d5905b0d81342d6c984163e6b854648
Princeme04 <ovankkusumadewa@gmail.com>
2025-03-10 10:11:00 +0700

    UPDATE

---
+++ codelab/modul 2/animal22.java
@@ -0,0 +1,21 @@
+public class animal22 {
+    public void animal2(){
+        String hewan2 = "Dog";
+        String type2 = "Mammal";
+        String Suara2 = "Woof-woof!!";
+
+        System.out.println("\nName: "+ hewan2);
+        System.out.println("Type: "+ type2);
+        System.out.println("Sound: "+ Suara2);
+}
+    public void animal1(){
+        String hewan1 = "Cat";
+        String type1 = "Mammal";
+        String suara1 = "Nyann~~";
+
+        System.out.println("\nName:"+ hewan1);
+        System.out.println("Type: "+ type1);
+        System.out.println("Sound: "+ suara1);
+    }
+}
+
--- codelab/modul 2/codelab2m2.java
+++ codelab/modul 2/codelab2m2.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class codelab2m2 {
-    public static void main (String[] args){
+    public static void displayInfo (){
+        Scanner norek = new Scanner(System.in);
+
+        System.out.println("Masukkan No Rek: ");
+        int pilihan = norek.nextInt();
+
+
 
     }
-}
+    public static void main (String[] args){
+        Scanner pilihan = new Scanner(System.in);
+
+        System.out.println("Pilih opsi: ");
+        System.out.println("1. Info");
+        System.out.println("2. Deposit");
+        System.out.println("3. Withdraw");
+        System.out.print("Masukkan pilihan: ");
+
+        int pilih = pilihan.nextInt();
+
+        switch(pilih){
+            case 1:
+                codelab2m2.displayInfo();
+                break;
+            case 2:
+//                hai;
+                break;
+            default:
+//        }
+
+
+    }
+}}
