a64a843dfa426691c243a804533499e3c4a453ea
Princeme04 <ovankkusumadewa@gmail.com>
2025-04-10 20:20:27 +0700

    update

--- tugas/modul 2/Admin.java
+++ tugas/modul 2/Admin.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Admin {
+    public static void loginAdmin() {
+        Scanner loginMin = new Scanner(System.in);
+
+        String userbenar = "admin506";
+        String passbenar = "pass506";
+        boolean berhasiltidak = true;
+
+        while (berhasiltidak) {
+            System.out.print("Masukan username: ");
+            String username = loginMin.nextLine();
+
+            System.out.print("Masukan password: ");
+            String password = loginMin.nextLine();
+
+            if (userbenar.equals(username) && passbenar.equals(password)) {
+                System.out.println("Login berhasil!!");
+                berhasiltidak = false;
+            } else {
+                System.out.println("Username atau password salah!!");
+                System.out.println("Mohon coba lagi");
+            }
+        }
+
+        loginMin.close();
+    }
 }
--- tugas/modul 2/Main.java
+++ tugas/modul 2/Main.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner pilih = new Scanner(System.in);
+
+        System.out.println("Pilih login:");
+        System.out.println("1. Admin");
+        System.out.println("2. Mahasiswa");
+        System.out.print("Masukkan pilihan: ");
+
+        int pilihan = pilih.nextInt();
+
+        switch (pilihan) {
+            case 1:
+                Admin.loginAdmin();
+                break;
+            case 2:
+                Mahasiswa.loginMhs();
+                break;
+            default:
+                System.out.println("Pilihan tidak valid");
+        }
+
+        pilih.close();
+        System.out.println("Process finished with exit code 0");
+    }
 }
